0823d4abcbecf35694068255d95cc4c4f603ed13
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:36:54 +0530

    Divison

---
+++ divisionFloat.c
@@ -0,0 +1,9 @@
+/*Write a program that declares two float variables and assigns them values.
+Then, use the division operator to divide the first variable by the second and print the result to the console with 3 decimal places.*/
+#include <stdio.h>
+void main(){
+    float a=30.24;
+    float b=15.39;
+    printf("%f",a/b);
+}
+
