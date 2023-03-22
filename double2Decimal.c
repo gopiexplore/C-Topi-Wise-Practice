2dadf9ef182fcc8460f1801abfa5d07c077feb41
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:07:42 +0530

    4th Question Doube 2 decimal palces

---
+++ double2Decimal.c
@@ -0,0 +1,8 @@
+//Write a program that declares and initializes a double variable with the value 3.14159. 
+//Then, print the variable to the console with 2 decimal places.
+#include <stdio.h>
+void main(){
+    double d=3.14159;
+    printf("%.2lf",d);
+}
+
