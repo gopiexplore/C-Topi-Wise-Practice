2e8f0476d90467364320943092c8866ee5a7a195
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:43:30 +0530

    Print Num1 to 10

---
+++ Num1to10.c
@@ -0,0 +1,10 @@
+/*Loops (for, while, do-while):
+
+Write a program that uses a for loop to print the numbers 1 through 10 to the console.*/
+#include <stdio.h>
+void main(){
+    int i;
+    for(i=0;i<=10;i++){
+        printf("%d ",i);
+    }
+}
