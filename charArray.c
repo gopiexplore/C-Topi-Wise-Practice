de2b70b7c0682945c08e2efc9e56881902cbd08b
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:06:20 +0530

    3 Rd Question CharArray

---
+++ charArray.c
@@ -0,0 +1,7 @@
+//Write a program that declares and initializes a char array with the value "hello world". Then, print the array to the console.
+#include <stdio.h>
+void main(){
+    char a[]={'h','e','l','l','o',' ','w','o','r','l','d'};
+    printf("%s",a);
+}
+
