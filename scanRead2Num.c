f5910ee25ce68e749c295b97a84683920557206b
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:39:39 +0530

    Take 2 Num input add print

---
+++ scanRead2Num.c
@@ -0,0 +1,9 @@
+/*Write a program that prompts the user to enter two integer values. 
+Then, use scanf to read in the values and use printf to print their sum to the console.*/
+#include <stdio.h>
+void main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d the sum is ",a+b);
+}
+
