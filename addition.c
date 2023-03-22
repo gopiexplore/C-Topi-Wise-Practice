c579e80f4652bcedc08d461b633968aec30938ba
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:35:53 +0530

    addition.c file

---
+++ addition.c
@@ -0,0 +1,10 @@
+/*Operators:
+
+Write a program that declares two integer variables and assigns them values. 
+Then, use the addition operator to add the two variables and print the result to the console.*/
+#include <stdio.h>
+void main(){
+    int a=10;
+    int b=20;
+    printf("%d",a+b);
+}
