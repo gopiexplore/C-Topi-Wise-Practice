549eb994910c3e4c5e2130fdf387935cb3720a83
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:16:25 +0530

    Constant Multiplied by Variable

---
+++ constantMulbyVar.c
@@ -0,0 +1,12 @@
+/*Constants:
+
+Write a program that declares a constant integer variable with the value 10. 
+Then, declare another integer variable and assign it the value of the constant variable multiplied by 5.
+Print the value of the new variable to the console.
+*/
+#include <stdio.h>
+void main(){
+    int a=10;
+    int b=5*a;
+    printf("%d ",b);
+}
