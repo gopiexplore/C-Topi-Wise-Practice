753b214d9c2202f140856bca3a54fcbf6e00f9f5
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:02:13 +0530

    Average of 3 Float Numbers

---
+++ averageof3Num.c
@@ -0,0 +1,10 @@
+//Write a program that declares three float variables and assigns them values. 
+//Then, calculate and print the average of the three values.
+#include <stdio.h>
+void main(){
+    float a=10.33;
+    float b=223.12;
+    float c=232.23;
+    printf("%f the average is ",a+b+c/3);
+}
+
