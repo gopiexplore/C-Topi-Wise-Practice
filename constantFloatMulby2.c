f3cea0b2137d4bf3ac380a69edf4c3d530e4503c
gopii8 <gopithammisetti6@gmail.com>
2023-03-22 18:33:49 +0530

    Constant Float Variable Multiplied by 2

---
+++ constantFloatMulby2.c
@@ -0,0 +1,12 @@
+/*
+Write a program that declares a constant float variable with the value 3.14.
+Then, declare another float variable and assign it the value of the constant variable multiplied by 2. 
+Print the value of the new variable to the console with 2 decimal places.*/
+#include <stdio.h>
+void main(){
+    float f=3.14;
+    float a=f*2;
+    printf("%f",a);
+}
+
+
