68c9f25100eee52e97165412f2885c099e8df3cb
gopii8 <gopithammisetti6@gmail.com>
2023-03-27 08:39:55 +0530

    comparison of 2 strings

---
+++ compare2strings.c
@@ -0,0 +1,15 @@
+//Write a program to compare two strings.
+#include <stdio.h>
+#include <string.h>
+void main(){
+    char a[]="Welcome ";
+    char b[]="Welcome     ";
+    printf("%d",strcmp(a,b));
+    
+}
+/*Here two string are equal getting 0
+if fist string is greater than the first string then it will return Positive integer.
+if first string is less then the second string it will return negative integer*/
+
+
+
