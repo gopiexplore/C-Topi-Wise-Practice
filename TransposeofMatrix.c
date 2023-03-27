7ff17d6d233cb6a9209af4415705cf09a5d3131f
gopii8 <gopithammisetti6@gmail.com>
2023-03-27 08:30:19 +0530

    Transpose of Matrix

---
+++ TransposeofMatrix.c
@@ -0,0 +1,19 @@
+//Write a program to find the transpose of a matrix.
+#include <stdio.h>
+void main(){
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int b[3][3];
+    int i,j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            b[j][i]=a[i][j];
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+}
+
